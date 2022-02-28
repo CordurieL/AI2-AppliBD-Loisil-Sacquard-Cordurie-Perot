695bcf9392aafae28f6f5a467b50243b437045d6
FurWaz <fur.waz06@gmail.com>
2022-02-28 15:55:04 +0100

    Premier commit
    Creation des fichiers de base du TD

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once "vendor/autoload.php";
+
+//connection a la base de donnees par le fichier conf.ini
+use Illuminate\Database\Capsule\Manager as DB;
+
+$db = new DB();
+$db->addConnection(parse_ini_file(dirname(__FILE__).'/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
