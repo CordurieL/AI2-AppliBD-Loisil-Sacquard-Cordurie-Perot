39e8c14c76c10cdc96f33c08545bcb65bc477a4e
FurWaz <fur.waz06@gmail.com>
2022-03-07 14:25:48 +0100

    execution TD2

--- Seance/index.php
+++ Seance/index.php
@@ -3,7 +3,7 @@ require_once "vendor/autoload.php";
 
 //connection a la base de donnees par le fichier conf.ini
 
-use AppliBD\controllers\TD1;
+use AppliBD\controllers\TD2;
 use Illuminate\Database\Capsule\Manager as DB;
 
 $db = new DB();
@@ -11,12 +11,12 @@ $db->addConnection(parse_ini_file(dirname(__FILE__).'/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
 
-TD1::Question1();
-
-TD1::Question2();
-
-TD1::Question3();
-
-TD1::Question4();
-
-TD1::Question5();
+TD2::Question1();
+TD2::Question2();
+TD2::Question3();
+TD2::Question4();
+TD2::Question5();
+TD2::Question6();
+TD2::Question7();
+TD2::Question8();
+TD2::Question9();
--- Seance/modules/controllers/TD2.php
+++ Seance/modules/controllers/TD2.php
@@ -8,38 +8,47 @@ use AppliBD\models\Platform;
 class TD2
 {
     public static function Question1() {
+        echo "Question1:\n";
 
     }
     
     public static function Question2() {
+        echo "Question2:\n";
 
     }
     
     public static function Question3() {
+        echo "Question3:\n";
 
     }
     
     public static function Question4() {
+        echo "Question4:\n";
 
     }
     
     public static function Question5() {
+        echo "Question5:\n";
 
     }
     
     public static function Question6() {
+        echo "Question6:\n";
 
     }
     
     public static function Question7() {
+        echo "Question7:\n";
 
     }
     
     public static function Question8() {
+        echo "Question8:\n";
 
     }
 
     public static function Question9() {
+        echo "Question9:\n";
 
     }
 }
