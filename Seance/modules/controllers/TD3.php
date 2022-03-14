56c346b39b02c592631f41ddae6b5147275f7b5b
CordurieL <82098372+CordurieL@users.noreply.github.com>
2022-03-14 14:43:13 +0100

    reprise des questions de TD4 precedents

--- Seance/modules/controllers/TD3.php
+++ Seance/modules/controllers/TD3.php
@@ -1,1 +1,39 @@
 <?php namespace AppliBD\controllers;
+
+use AppliBD\models\Company;
+use AppliBD\models\Game;
+use AppliBD\models\Genre;
+use AppliBD\models\Personnage;
+use AppliBD\models\Platform;
+
+class TD3
+{
+    public static function Question1()
+    {
+        $res = Game::all()->get();
+    }
+
+    public static function Question2()
+    {
+        $res = Game::where("name", "like", "%Mario%")->get();
+    }
+
+    public static function Question3()
+    {
+        $res = Game::where("name", "like", "Mario%")->with("personnages")->get() ;
+    }
+
+    public static function Question4()
+    {
+        $jeux = Game::where("name", "like", "Mario%")->get()->filter(function ($jeu) {
+            $contient = false;
+            foreach ($jeu->ratings as $rating) {
+                if (strpos($rating->name, "3+") != false) {
+                    $contient = true;
+                    break;
+                }
+            }
+            return $contient;
+        });
+    }
+}
