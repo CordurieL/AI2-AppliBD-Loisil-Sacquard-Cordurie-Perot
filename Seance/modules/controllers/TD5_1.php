45a2340717e8afb7014cb5ebb0a12fcba8a08456
FurWaz <fur.waz06@gmail.com>
2022-03-30 14:38:35 +0200

    Update TD5_1.php

--- Seance/modules/controllers/TD5_1.php
+++ Seance/modules/controllers/TD5_1.php
@@ -6,18 +6,11 @@ class TD5_1 {
     public static function getGame($id) {
         $game = Game::find($id);
         if (!$game) return [];
+        return TD5_1::make_json($game);
+    }
 
-        // {
-        //     "id": 35444,
-        //     "name": "War Thunder",
-        //     "alias": "World of Planes",
-        //     "deck": "War Thunder is a free-to-play air combat game set in the World War II and Korean War eras.
-        //     Naval and land battles are planned additions.",
-        //     "description": "...",
-        //     "original_release_date": "2013-06-30 00:00:00"
-        // }
-
-        $data = [
+    public static function make_json($game) {
+        return [
             "id" => $game->id,
             "name" => $game->name,
             "alias" => $game->alias,
@@ -25,6 +18,5 @@ class TD5_1 {
             "description" => $game->description,
             "original_release_date" => $game->original_release_date
         ];
-        return $data;
     }
 }
