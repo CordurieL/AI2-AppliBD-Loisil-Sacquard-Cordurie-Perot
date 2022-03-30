b7b87c86e2d43dc0749d1b8d914a5313fb4d5da8
CordurieL <82098372+CordurieL@users.noreply.github.com>
2022-03-30 14:13:44 +0200

    Question 2 du TD4 mais ça marche pas vraiment

--- Seance/modules/controllers/TD4.php
+++ Seance/modules/controllers/TD4.php
@@ -5,19 +5,39 @@ use AppliBD\models\Comment;
 
 class TD4
 {
-
-    
-    static function Question1() {
+    public static function Question1()
+    {
         $max = User::max('id');
         $min = User::min('id');
         $id = readline("ID de l'utilisateur (entre ".$min." et ".$max.") : ");
         $user = User::find(max($min, min($max, $id)));
         $comments = $user->comments()->orderBy("created_at", "desc")->get();
         echo "Commentaires de l'utilisateur " . $user->name . " " . $user->surname . " :\n";
-        foreach($comments as $comment) {
+        foreach ($comments as $comment) {
             $date = $comment->created_at->format("d/m/Y");
             echo "  " . $comment->title . " (" .$date. ")\n";
             echo "    " . $comment->content . "\n";
+        }
+    }
+
+    /*SELECT * FROM user INNER JOIN comment ON comment.user_id = user.id group by user.id HAVING count(comment.user_id) > 5; */
+    // Laravel 
+
+    public static function Question2()
+    {
+        /*$users = User::with(["comments" => function ($comment) {
+            $comment->groupBy('user_id')->havingRaw("count(*) > 5")->get();
+        }]);*/
+        $users = User::where("id", ">", 24950)->get()->filter(function ($user) {
+            $countComments =  $user->comments->count();
+            if ($countComments >= 15) {
+                return $user->name;
+            }
+        });
+
+        echo "Noms des utilisateurs ayant plus de 5 commentaires\n";
+        foreach ($users as $user) {
+            echo "    " . $user->name . "\n";
         }
     }
 }
