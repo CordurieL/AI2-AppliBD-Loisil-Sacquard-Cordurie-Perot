e7316e8f69178c81a6a6766ea03cd2cf9f8d56fc
FurWaz <fur.waz06@gmail.com>
2022-03-30 14:11:38 +0200

    ajout slim avec composer

--- Seance/index.php
+++ Seance/index.php
@@ -3,7 +3,7 @@ require_once "vendor/autoload.php";
 
 //connection a la base de donnees par le fichier conf.ini
 
-use AppliBD\controllers\TD4;
+use AppliBD\controllers\TD5;
 use Illuminate\Database\Capsule\Manager as DB;
 
 $db = new DB();
@@ -11,11 +11,12 @@ $db->addConnection(parse_ini_file(dirname(__FILE__).'/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
 
-while (true) {
-    $a = '0';
-    $a = readline("Question numero 1 - 2 (ou 0 pour quitter) : ");
-    if ($a == '0')
-        break;
-    $q = "Question".$a;
-    TD4::$q();
-}
+// while (true) {
+//     $a = '0';
+//     $a = readline("Question numero 1 - 2 (ou 0 pour quitter) : ");
+//     if ($a == '0')
+//         break;
+//     $q = "Question".$a;
+//     TD4::$q();
+// }
+
