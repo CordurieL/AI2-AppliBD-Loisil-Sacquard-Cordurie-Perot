9dbbb80217ccb2a6d3cd24226a04f3d084f6de90
FurWaz <fur.waz06@gmail.com>
2022-03-30 16:38:03 +0200

    Update TD5_1.php

--- Seance/modules/controllers/TD5_1.php
+++ Seance/modules/controllers/TD5_1.php
@@ -1,20 +1,17 @@
 <?php namespace AppliBD\controllers;
 
+use AppliBD\models\Comment;
 use AppliBD\models\Game;
+use AppliBD\models\User;
 
-class TD5_1
-{
-    public static function getGame($id)
-    {
+class TD5_1 {
+    public static function getGame($id) {
         $game = Game::find($id);
-        if (!$game) {
-            return [];
-        }
+        if (!$game) return [];
         return TD5_1::make_json($game);
     }
 
-    public static function make_json($game)
-    {
+    public static function make_json($game) {
         return [
             "game" => [
                 "id" => $game->id,
@@ -32,8 +29,7 @@ class TD5_1
         ];
     }
 
-    public static function getPlatforms($game)
-    {
+    public static function getPlatforms($game) {
         $res = [];
         $platforms = $game->platforms;
         foreach ($platforms as $platform) {
@@ -48,8 +44,7 @@ class TD5_1
         return $res;
     }
 
-    public static function getGameCharacters($id)
-    {
+    public static function getGameCharacters($id) {
         $characters = Game::find($id)->personnages;
         $res = [];
         foreach ($characters as $char) {
@@ -66,8 +61,31 @@ class TD5_1
         return ["characters" => $res];
     }
 
-    public static function addGameComment($game_id, $body)
-    {
-        var_dump(json_decode($body, true));
+    public static function addGameComment($game_id, $body) {
+        $data = json_decode($body, true);
+        $user = User::find($data["email"]);
+        if (!$user) return null;
+        $comment = Comment::create([
+            "title" => $data["title"],
+            "user_id" => $user->id,
+            "game_id" => $game_id,
+            "content" => $data["content"]
+        ]);
+        return TD5_1::make_comment($comment);
+    }
+
+    public static function make_comment($comment) {
+        return [
+            "comment" => [
+                "id" => $comment->id,
+                "title" => $comment->title,
+                "content" => $comment->content,
+                "user_id" => $comment->user_id,
+                "created_at" => $comment->created_at
+            ],
+            "links" => [
+                "self" => ["href" => Container::getContainer()->router->pathFor("comments", ["id" => $comment->id])]
+            ]
+        ];
     }
 }
