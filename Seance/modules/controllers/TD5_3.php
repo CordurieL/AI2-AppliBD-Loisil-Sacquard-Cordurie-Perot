7f11986e60927628af50ba9130c3626c7330ac55
MathisPerot <90915143+MPEROT54@users.noreply.github.com>
2022-03-30 15:40:49 +0200

    td5
    getGameComments

--- Seance/modules/controllers/TD5_3.php
+++ Seance/modules/controllers/TD5_3.php
@@ -3,6 +3,18 @@
 
 class TD5_3 {
     public static function getGameComments($id) {
-        
+		// http://www.gamepedia.net/api/games/2/comments
+        $res = [];
+        $comments = Comment::where("game_id", "=", $id)->get();
+        foreach ($comments as $comment) {
+            array_push($res, [
+                "id" => $comment->id,
+                "title" => $comment->title,
+                "content" => $comment->content,
+                "user_id" => $comment->user_id,
+                "href" => "/api/games/{$comment->id}/comments"
+            ]);
+        }
+        return $res;
     }
 }
