f9522c7027b6b1f9e67c8e7a81f6e485d549a7b1
FurWaz <fur.waz06@gmail.com>
2022-03-30 14:57:45 +0200

    Ajout links dans games

--- Seance/modules/controllers/TD5_1.php
+++ Seance/modules/controllers/TD5_1.php
@@ -11,12 +11,18 @@ class TD5_1 {
 
     public static function make_json($game) {
         return [
-            "id" => $game->id,
-            "name" => $game->name,
-            "alias" => $game->alias,
-            "deck" => $game->deck,
-            "description" => $game->description,
-            "original_release_date" => $game->original_release_date
+            "game" => [
+                "id" => $game->id,
+                "name" => $game->name,
+                "alias" => $game->alias,
+                "deck" => $game->deck,
+                "description" => $game->description,
+                "original_release_date" => $game->original_release_date
+            ],
+            "link" => [
+                "comments" => ["href" => "/api/games/{$game->id}/comments"],
+                "characters" => ["href" => "/api/games/{$game->id}/characters"]
+            ]
         ];
     }
 }
